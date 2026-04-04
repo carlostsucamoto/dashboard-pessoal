aff0f52368843725a4c56040ecfbf41acd0c66c7
carlostsucamoto <caduchiarelli@hotmail.com>
2026-04-04 00:50:13 -0300

    feat: adicionado a função sortear no backlog

--- js/jogos.js
+++ js/jogos.js
@@ -2,6 +2,7 @@ const meuBotao = document.getElementById("adicionar-jogo");
 const nomeJogo = document.getElementById("nome-jogo");
 const estiloJogo = document.getElementById("estilo-jogo");
 const vontadeJogo = document.getElementById("vontade-jogo");
+const sortearJogo = document.getElementById("botao-sortear");
 let listaJogos = JSON.parse(localStorage.getItem("meuBacklog")) || [];
 meuBotao.addEventListener("click", function () {
 let nomeDigitado = nomeJogo.value;
@@ -12,8 +13,19 @@ let jogoNovo = {
     estilo: estiloDigitado,
     vontade: vontadeDigitado
 };
+
 listaJogos.push(jogoNovo);
 let listaConvertida = JSON.stringify(listaJogos);
 localStorage.setItem("meuBacklog", listaConvertida);
 atualizarLista()
+})
+sortearJogo.addEventListener("click", function (event) {
+    if( listaJogos.length === 0) {
+        alert("Adicione um jogo antes de sortear");
+        return;
+    }
+    let jogoAleatorio = Math.floor (Math.random() * listaJogos.length);
+    let jogoEscolhido = listaJogos[jogoAleatorio];
+    
+sortearJogo.innerHTML = "Jogue: " + jogoEscolhido.nome;
 })
